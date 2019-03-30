f757ee8e1221c67f2878cf93d98d9b841febf394
Desktop-Hero <doctormcnaughton@gmail.com>
2019-03-30 17:00:30 -0400

    Added third video project

---
+++ Level3 Homework-4-1-19/Assets/HealthBarScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+
+    public Image healthBar;
+    float maxHealth = 100f;
+    public static float health;
+    public Text healthText;
+
+    void Start()
+    {
+        healthBar = GetComponent<Image> ();
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;
+        healthText.text = "HP : " + health / 10;
+    }
+}
